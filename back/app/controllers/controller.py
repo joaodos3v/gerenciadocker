30908cf9bf73cfca2f78435e5e4e0852db754126
Vinicius Emanoel Andrade <viniciusemanoelandrade@hotmail.com>
2019-09-19 01:21:49 -0300

    [Python] Foi criada uma rota que inicia o container, a distribuicao utilizada é passada como parametro na instancia do objeto container

--- back/app/controllers/controller.py
+++ back/app/controllers/controller.py
@@ -1,6 +1,15 @@
 from flask import jsonify
 from app import app
+from app.models.container import Container
 
 @app.route("/")
 def index():
     return '<h3>O servidor está em execução</h3>'
+
+@app.route("/container/iniciar")
+def iniciar():
+    container = Container('ubuntu')
+    container.iniciar()
+    return jsonify({"retorno":1})    
+    
+    
