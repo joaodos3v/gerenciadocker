0de5996e343a41ba9403d731cda0ca69500c172d
Joao Vitor Veronese Vieira <joaovitorvv@gmail.com>
2019-09-29 19:26:00 -0300

    [ReactJS] Incluindo template no sistema  &  Utilizando Landing Page de exemplo

--- front/src/index.js
+++ front/src/index.js
@@ -1,9 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import LandingPage from './LandingPage';
+
+// styles for this kit
+import "./assets/css/bootstrap.min.css";
+import "./assets/scss/now-ui-kit.scss";
+
+import * as serviceWorker from './serviceWorker';
+/**
+ * Descomentar para voltar ao status inicial do projeto
+ * 
+ * import App from './App';
+ * ReactDOM.render(<App />, document.getElementById('root'));
+ */
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Switch>
+      <Route
+        path="/"
+        render={props => <LandingPage {...props} />}
+      />
+    </Switch>
+  </BrowserRouter>,
+  document.getElementById("root")
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
