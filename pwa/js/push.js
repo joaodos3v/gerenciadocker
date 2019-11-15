46bc21a65d9158d64283c1e2c56d809921affa53
Joao Vitor Veronese Vieira <joaovitorvv@gmail.com>
2019-11-15 18:08:52 -0300

    [PWA] Atualizando registration das push notifications (problema com path)

--- pwa/js/push.js
+++ pwa/js/push.js
@@ -6,33 +6,44 @@ let getDate = () => {
     return `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}, às ${date.getHours()}h${date.getMinutes()}`;
 };
 
-const messaging = firebase.messaging();
 
-// Requisita um token para receber notificações
-let getToken = () => {
-    messaging.getToken()
-        .then(currentToken => {
-            if (currentToken) {
-                console.log("[PushNotification] Token Atual: ", currentToken);
-                return currentToken;
-            } else {
-                console.warn("[PushNotification] Nenhum id disponível!");
-            }
-        })
-        .catch(err => console.warn("[PushNotification] Get Token Error: ", err));
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+            .register("firebase-messaging-sw.js")
+            .then(registration => {
+                const messaging = firebase.messaging();
+
+                messaging.useServiceWorker(registration);
+
+                // Requisita um token para receber notificações
+                let getToken = () => {
+                    messaging.getToken()
+                        .then(currentToken => {
+                            if (currentToken) {
+                                console.log("[PushNotification] Token Atual: ", currentToken);
+                                return currentToken;
+                            } else {
+                                console.warn("[PushNotification] Nenhum id disponível!");
+                            }
+                        })
+                        .catch(err => console.warn("[PushNotification] Get Token Error: ", err));
+                }
+
+
+                // Solicita permissão ao usuário para exibir notificações
+                messaging.requestPermission(registration)
+                    .then(() => {
+                        console.log("[PushNotification] Permission granted!");
+                        getToken();
+                    });
+
+
+                // "Escuta" mensagens locais (com o aplicativo aberto)
+                messaging.onMessage((payload) => {
+                    console.log("[PushNotification] Local Message: ", payload);
+                    addFailure(payload);
+                });
+
+            })
+            .catch(err => console.warn('Err: ', err));
 }
-
-
-// Solicita permissão ao usuário para exibir notificações
-messaging.requestPermission()
-    .then(() => {
-        console.log("[PushNotification] Permission granted!");
-        getToken();
-    });
-
-
-// "Escuta" mensagens locais (com o aplicativo aberto)
-messaging.onMessage((payload) => {
-    console.log("[PushNotification] Local Message: ", payload);
-    addFailure(payload);
-});
