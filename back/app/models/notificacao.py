924897b0dfdfad53ed1eedf3c77002d75e8cc125
Vinicius Emanoel Andrade <viniciusemanoelandrade@hotmail.com>
2019-11-21 16:39:35 -0300

    [Python]
    Notificações sendo enviadas com sucesso, porém, será necessário atualizar manualmente o atributo to do objeto que é serializado

--- back/app/models/notificacao.py
+++ back/app/models/notificacao.py
@@ -20,13 +20,16 @@ class Notificacao:
 
     def enviar(self):
         url = "https://fcm.googleapis.com/fcm/send"
-        headers = {'Authorization': 'key=AAAAkzzank4:APA91bGNXhwsn9OeBSYvOUvMluQH5TBXxPo9XZUJdy78Kv12Q6w_OTGpDhG9-6GbTdcCXK83D4vIa2qao4cs-nTjKgH8uC_RZvY5svoo1cOMG8Jmfs1AzhrA9Z3ZqowJW9p9GF35txZc'}
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': 'key=AAAAofhTans:APA91bH4loh0DwPxz3A_yQOp2ZVKP67coiYKvF6_ACUU_vcc6HmaWciDXl0j07xq9gJ6NkqaikId6X9Gj0YAA1phY-ZOCrzge_2Z7gYqFc-eE7MhZv8TjIB6UJfEHH887MIKeYrPKeOJ'
+            }
         notificacao = {
-            "to": "cocU4X_PrhwDGEtX2Hqfif:APA91bEe7oXmu12thGldMf1gbsKQU8l23RtKurflHl9pUIAJydjuEyNaDUKAI3_DzHQzNKua3gODIhmx1FZmbrjPCH5pL740nc9DiFVF8zLQtXimWHvQ4pehkS_abjyxizN-FJjgZPOa",
+            "to": "efMCGFZO2TTRq9l0OfMstF:APA91bFa--jn8LtmsGsAPwuQ6B_SsvH6UC4Q7EsYZcqJfGIF7ifxLzVB5S5GnMNQtHiRFmpucU99m2t_t7oOpUV8V0As3zoL0cN40Sb1hT-Rt9gqIBgT1AsoruFobL9xSG-yYmn24EsK",
             "notification": {
                 "title": "%s" % self.titulo,
                 "body": "%s" % self.mensagem,
-         		"click_action": "http://localhost:8080",
+         		"click_action": "https://joao-vieira.github.io/gerenciadocker/pwa/",
  		        "icon": "http://localhost:8080/static/img/icons/android-chrome-512x512.png"
             }
         }
