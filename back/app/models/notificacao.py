5700bcd56cd7291c62702a7dd27c643039f636d3
Vinicius Emanoel Andrade <viniciusemanoelandrade@hotmail.com>
2019-11-21 16:01:03 -0300

    [Python]
    testes com a notificação, ela deve aparecer na PWA

--- back/app/models/notificacao.py
+++ back/app/models/notificacao.py
@@ -22,7 +22,7 @@ class Notificacao:
         url = "https://fcm.googleapis.com/fcm/send"
         headers = {'Authorization': 'key=AAAAkzzank4:APA91bGNXhwsn9OeBSYvOUvMluQH5TBXxPo9XZUJdy78Kv12Q6w_OTGpDhG9-6GbTdcCXK83D4vIa2qao4cs-nTjKgH8uC_RZvY5svoo1cOMG8Jmfs1AzhrA9Z3ZqowJW9p9GF35txZc'}
         notificacao = {
-            "to": "d0XqFNb7Xf8Q5xzLZSWpI1:APA91bHawRUL48YW4ZY4hFbzGukq4NvbHX9ZMBJYusOYS_VZX_gPM_Hp6XWkohlb_9e6r6-3H4Mky9cxNCB1bxMwJqrcMc5dsMDXxK7KRqxbYJT126GwKdUqrO0QuVNcOSZ61pBSf2kY",
+            "to": "cocU4X_PrhwDGEtX2Hqfif:APA91bEe7oXmu12thGldMf1gbsKQU8l23RtKurflHl9pUIAJydjuEyNaDUKAI3_DzHQzNKua3gODIhmx1FZmbrjPCH5pL740nc9DiFVF8zLQtXimWHvQ4pehkS_abjyxizN-FJjgZPOa",
             "notification": {
                 "title": "%s" % self.titulo,
                 "body": "%s" % self.mensagem,
